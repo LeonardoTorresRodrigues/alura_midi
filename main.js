801c523d7bdf094d3214e7036af47fb2fe461e61
Leonardo Rodrigues <leonardo.t.rodri@gmail.com>
2022-06-09 12:11:16 -0300

    Refatorando função e declarando variável de listas

--- main.js
+++ main.js
@@ -1,1 +1,6 @@
-document.querySelector('.tecla_pom');
+function playPom() {
+  document.querySelector('#som_tecla_pom').play();
+}
+ const keysList = document.querySelectorAll('.tecla');
+
+ keysList[0].onclick = playPom;
