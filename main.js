ddccbbb86e35d86464b3ad8bcc5e189ee98c4e8e
Leonardo Rodrigues <leonardo.t.rodri@gmail.com>
2022-06-27 19:11:31 -0300

    Adicionando funções de verificação de teclas apertadas

--- main.js
+++ main.js
@@ -1,14 +1,34 @@
 function playAudio(idAudio) {
-  document.querySelector(idAudio).play();
-}
+  const element = document.querySelector(idAudio);
+
+  if(element === null) {
+    alert('Elemento não encontrado');
+  };
+
+  if(element != null) {
+    if(element.localName === 'audio') {
+      element.play();
+    };
+  };
+};
 const keysList = document.querySelectorAll('.tecla');
 
-for (let countdown = 0; countdown < keysList.length; countdown++) {
-  const key = keysList[countdown];
+for (let i = 0; i < keysList.length; i++) {
+  const key = keysList[i];
   const instrument = key.classList[1];
   const idAudio = `#som_${instrument}`;
 
   key.onclick = function () {
     playAudio(idAudio);
+  };
+
+  key.onkeydown = function (event) {
+    if(event.code === "Enter" || event.code === "Space") {
+      key.classList.add('ativa');
+    };
+  };
+
+  key.onkeyup = function () {
+    key.classList.remove('ativa');
   };
 };
